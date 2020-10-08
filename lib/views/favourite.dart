51fa2a553811f8f1edcf7f7a944339893cedf60d
chetan177 <chetanpandey177@gmail.com>
2020-10-08 17:54:29 +0530

    view for serach profile and favourite created

---
+++ lib/views/favourite.dart
@@ -0,0 +1,16 @@
+import 'package:GameZooApp/model/colors.dart';
+import 'package:flutter/material.dart';
+
+class Favourite extends StatefulWidget {
+  @override
+  _FavouriteState createState() => _FavouriteState();
+}
+
+class _FavouriteState extends State<Favourite> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+    );
+  }
+}
---
+++ lib/views/profile.dart
@@ -0,0 +1,16 @@
+import 'package:GameZooApp/model/colors.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Primary,
+    );
+  }
+}
